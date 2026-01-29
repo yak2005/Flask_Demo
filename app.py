62bdb595b69a3637b5b7962967b8b55da512ba9c
Yakshith Shettigar <yakshithshettigar8@gmail.com>
2026-01-29 12:52:09 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import joblib
+from flask import Flask, render_template, request
+from sklearn.neighbors import KNeighborsClassifier
+
+app = Flask(__name__)
+
+
+X = [[20], [30], [40], [50], [55], [65], [75]]
+y = ['Fail', 'Fail', 'Fail', 'Pass', 'Pass', 'Pass', 'Pass']
+
+
+model = KNeighborsClassifier(n_neighbors=3)
+model.fit(X, y)
+
+
+joblib.dump(model, "model.pkl")
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    prediction = None
+    if request.method == "POST":
+        num = float(request.form.get("number"))
+        prediction = model.predict([[num]])[0]  # get value
+    return render_template("index.html", pred=prediction)
+
+if __name__ == "__main__":
+    app.run(debug=True)
